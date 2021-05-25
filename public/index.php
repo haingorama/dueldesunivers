2056937bad259cb5aa5b504ed22045771b48b5e1
haingorama <haingo.ramaroson.pro@gmail.com>
2021-05-25 11:23:36 +0200

    Controller login/logout/register

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use Entity\User;
 use Entity\Category;
 use ludk\Persistence\ORM;
 use Controller\HomeController;
+use Controller\AuthController;
 
 require __DIR__ . "/../vendor/autoload.php";
 
@@ -20,57 +21,22 @@ $categoryRepo = $orm->getRepository(Category::class);
 
 $action = $_GET["action"] ?? "display";
 switch ($action) {
+
     case 'register':
-        if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['passwordRetype'])) {
-            $usersWithThisUsername = $userRepo->findBy(["nickname" => $_POST["username"]]);
-            if (count($usersWithThisUsername) > 0) {
-                $errorMsg = "Nickname already used.";
-            } else if ($_POST['password'] != $_POST['passwordRetype']) {
-                $errorMsg = "Passwords are not the same.";
-            } else if (strlen(trim($_POST['password'])) < 4) {
-                $errorMsg = "Your password should have at least 4 characters.";
-            } else if (strlen(trim($_POST['username'])) < 4) {
-                $errorMsg = "Your nickame should have at least 4 characters.";
-            }
-            if ($errorMsg) {
-                include "../templates/RegisterForm.php";
-            } else {
-                $newUser = new User();
-                $newUser->nickname = $_POST['username'];
-                $newUser->password = $_POST['password'];
-                $manager->persist($newUser);
-                $manager->flush();
-                $_SESSION['user'] = $newUser;
-                header('Location: ?action=display');
-            }
-        } else {
-            include "../templates/RegisterForm.php";
-        }
+        $controller = new AuthController();
+        $controller->register();
         break;
+
     case 'logout':
-        if (isset($_SESSION['user'])) {
-            unset($_SESSION['user']);
-        }
-        header('Location: ?action=display');
+        $controller = new AuthController();
+        $controller->logout();
         break;
+
     case 'login':
-        if (isset($_POST['username']) && isset($_POST['password'])) {
-            $criteriaWithloginAndPawword = [
-                "nickname" => $_POST['username'],
-                "password" => $_POST['password']
-            ];
-            $usersWithThisNicknameAndPassword = $userRepo->findBy($criteriaWithloginAndPawword);
-            if (count($usersWithThisNicknameAndPassword) == 1) {
-                $_SESSION['user'] = $usersWithThisNicknameAndPassword[0];
-                header('Location: ?action=display');
-            } else {
-                $errorMsg = "Wrong login and/or password.";
-                include "../templates/loginForm.php";
-            }
-        } else {
-            include "../templates/loginForm.php";
-        }
+        $controller = new AuthController();
+        $controller->login();
         break;
+
     case 'new':
         $categories = $categoryRepo->findAll();
         if (
@@ -116,6 +82,7 @@ switch ($action) {
             include "../templates/CreateForm.php";
         }
         break;
+
     case 'display':
     default:
         $controller = new HomeController();
